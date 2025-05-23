e5c94cbe0750713ac43288a73394a4f51744b0e4
dave21-py <dgedd236@students.bju.edu>
2025-05-23 14:46:50 -0400

    Updated Screen

---
+++ app/src/main/java/org/example/MainWindow.java
@@ -0,0 +1,124 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
+
+
+public class MainWindow {
+
+    @FXML
+    private Button btnOn;
+
+    @FXML
+    private Button btnOff;
+
+    @FXML
+    private Button volUp;
+
+    @FXML
+    private Button volDown;
+
+    @FXML
+    private Button btnHdmi;
+
+    @FXML
+    private Button btnWireless;
+
+    @FXML
+    private void onOnClicked(ActionEvent event){
+        if (!systemOn){
+            btnOn.setStyle("-fx-background-color: red; -fx-text-fill: black;");
+            systemOn = true;
+
+            btnHdmi.setDisable(false);
+            btnWireless.setDisable(false);
+            volDown.setDisable(false);
+            volUp.setDisable(false);
+
+            
+
+
+        }
+    }
+
+    @FXML
+    private void onOffClicked(ActionEvent event){
+        btnOn.setStyle("");
+        systemOn = false;
+
+        btnHdmi.setDisable(true);
+        btnWireless.setDisable(true);
+        volDown.setDisable(true);
+        volUp.setDisable(true);
+
+        
+
+
+    }
+
+    @FXML
+    private void onVolUpClicked(){
+        volUp.setStyle("-fx-background-color: red; -fx-text-fill: black;");
+        System.out.println("Volume Increased!");
+        PauseTransition pause = new PauseTransition(Duration.millis(100));
+        pause.setOnFinished(e -> volUp.setStyle(""));
+        pause.play();
+    }
+
+    @FXML
+    private void onVolDownClicked(){
+        volDown.setStyle("-fx-background-color: red; -fx-text-fill: black;");
+        System.out.println("Volume Decreased!");
+        PauseTransition pause = new PauseTransition(Duration.millis(100));
+        pause.setOnFinished(e -> volDown.setStyle(""));
+        pause.play();
+    }
+
+    @FXML
+    private void onHdmiClicked(){
+        if(!currentMode.equals("HDMI")){
+            currentMode = "HDMI";
+            btnHdmi.setStyle("-fx-background-color: red; -fx-text-fill: black;");
+            btnWireless.setStyle("");
+            System.out.println("HDMI MODE");
+    }
+}
+
+    @FXML
+    private void onWirelessClicked(){
+        if(!currentMode.equals("WIRELESS")){
+            currentMode = "WIRELESS";
+            btnWireless.setStyle("-fx-background-color: red; -fx-text-fill: black;");
+            btnHdmi.setStyle("");
+            System.out.println("WIRELESS MODE");
+    }
+}
+
+    private void showMessage(String text){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Controller FEEDBACK");
+        alert.setHeaderText(null);
+        alert.setContentText(text);
+        alert.showAndWait();
+
+    }
+
+    private boolean systemOn = false;
+
+    private String currentMode = "";
+
+
+
+    @FXML
+    private void initialize() {
+        btnHdmi.setDisable(true);
+        btnWireless.setDisable(true);
+        volDown.setDisable(true);
+        volUp.setDisable(true);
+    }
+}
+        
